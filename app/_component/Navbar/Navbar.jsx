0bf835d5a2bac33eb36c1d02766b02f77c2e0399
iboke2 <fazexxibrahim09@gmail.com>
2024-05-08 00:28:14 +0300

    update

--- app/_component/Navbar/Navbar.jsx
+++ app/_component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import logo from "../../img/The First logo design for WeSmartt coloring-05.webp";
+import logo from "../../img/LightWESMART-11.png";
 import Link from "next/link";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
@@ -40,6 +40,7 @@ function Navbar({ active }) {
               alt="Logo"
               width={140}
               height={200}
+              className="tazbit"
             />
           </Link>
           <nav className="hidden lg:block ">
