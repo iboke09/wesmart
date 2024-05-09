42ad55b8c0b5e94811e962c48bd8657a8cea6d20
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-05-09 21:24:36 +0300

    update

--- app/_component/Navbar/ResponsiveMenu.jsx
+++ app/_component/Navbar/ResponsiveMenu.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { VscFoldRight, VscFoldUp, VscChevronRight } from "react-icons/vsc";
 import { AiOutlineClose } from "react-icons/ai";
+import DarkMode from "../DarkMode/darkMode";
+
 function ResponsiveMenu({ showMenu, toggleMenu }) {
   return (
     <>
