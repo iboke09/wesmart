8a418c626023406126e8dae37b184b660a2837f5
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-05-23 18:45:10 +0300

    update

--- app/_component/Incaz/Incaz.jsx
+++ app/_component/Incaz/Incaz.jsx
@@ -8,7 +8,7 @@ import React from "react";
 export default function Component() {
   return (
     <div className="relative w-full h-[80vh] overflow-hidden youy">
-      <video autoPlay muted loop style={{height:"100%"}} className="videoT">
+      <video autoPlay muted loop playsinline  style={{height:"100%"}} className="videoT">
         <source src={"/_next-videos/Technology.mp4"} type="video/mp4" />
       </video>
       <span className="absolute inset-0 w-full h-full object-cover rounded-md bg-muted" />
