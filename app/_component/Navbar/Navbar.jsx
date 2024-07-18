55229df833d1b05b19ab88efd55e3835e9c316f4
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-07-18 12:58:37 +0300

    update

--- app/_component/Navbar/Navbar.jsx
+++ app/_component/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import logo from "../../img/LogoForDarkBack-24-24-02.webp";
+import logo from "../../img/LogoForDarkBackFinal-02.webp";
 import Link from "next/link";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
