61dd791ff94cc0801edb5f3f2c0ff417192508b9
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-07-24 16:10:35 +0300

    update

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
-import img from "../img/Group 1614Flip.svg";
-import img2 from "../img/howard-bouchevereau-nrbZ80NVx1o-unsplash (1).jpg";
+import img from "../img/Researching-amico.svg";
+import img1 from "../img/Man thinking-bro.svg";
+import img2 from "../img/Shrug-bro.svg";
 import Footer from "../_component/Footer/Footer";
 
 function About() {
@@ -9,7 +10,7 @@ function About() {
     <>
       <div className="kura">
         <div
-          style={{ height: "100vh", top: "100px", position: "relative" }}
+          style={{ height: "64vh", top: "100px", position: "relative" }}
           className="container"
         >
           <section>
@@ -45,6 +46,40 @@ function About() {
               </div>
             </div>
           </section>
+        </div>
+      </div>
+      <div className="rest">
+        <div className="container re">
+          <div className="box">
+            <div>
+              <Image src={img} alt="" />
+              <h1>Who We Are</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Distinctio quia nobis autem necessitatibus illo. Mollitia!
+            </p>
+          </div>
+          <div className="box">
+            <div>
+              <Image src={img1} alt="" />
+              <h1>What We Do</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              molestias facilis est labore ipsam eveniet!
+            </p>
+          </div>
+          <div className="box">
+            <div>
+              <Image src={img2} alt="" />
+              <h1>Why We Do It</h1>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos aut
+              at, non magni laborum dolorum.
+            </p>
+          </div>
         </div>
       </div>
       <Footer />
