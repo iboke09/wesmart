72036b69fa17a15d56fe5a74702ade971eec530a
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-07-25 18:44:11 +0300

    update

--- app/services/page.js
+++ app/services/page.js
@@ -36,240 +36,15 @@ export default function Services() {
   return (
     <>
       <div style={{}}>
-        {/* <section
-          className="bg-gray-900 text-white"
-          style={{
-            height: "100vh",
-            position: "relative",
-            top: "0",
-            paddingTop: "75px",
-          }}
-        >
-          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-            <div className="mx-auto max-w-lg text-center">
-              <h2 className="text-3xl font-bold sm:text-4xl">
-                Kickstart your marketing
-              </h2>
-
-              <p className="mt-4 text-gray-300">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae
-                aperiam fugit consequuntur saepe laborum.
-              </p>
-            </div>
-
-            <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-
-              <a
-                className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-blue-600 hover:shadow-blue-600/30"
-                href="#"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-10 texoI"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-
-                <h2 className="mt-4 text-xl font-bold text-white">
-                  Digital campaigns
-                </h2>
-
-                <p className="mt-1 text-sm text-gray-300">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
-                  quo possimus adipisci distinctio alias voluptatum blanditiis
-                  laudantium.
-                </p>
-              </a>
-            </div>
-
-            <div className="mt-12 text-center">
-              <a
-                href="#"
-                className="inline-block rounded texoI px-12 py-3 text-sm font-medium text-white  transition hover:bg-cyan-950 focus:outline-none focus:ring focus:ring-yellow-400"
-              >
-                Get Started Today
-              </a>
-            </div>
-          </div>
-        </section> */}
         <div
           className="py-20"
           style={{
             backgroundImage:
-              "linear-gradient(90deg,rgba(0, 194, 255, 1) 0%,rgba(0, 51, 128, 1) 100%)",paddingTop:'200px'
+              "linear-gradient(90deg,rgba(0, 194, 255, 1) 0%,rgba(0, 51, 128, 1) 100%)",
+            paddingTop: "200px",
           }}
         >
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style={{color:"var(--blawh-color)"}}>
             <h2 className="text-4xl font-extrabold text-white text-center mb-12">
               Our Services
             </h2>
@@ -277,15 +52,16 @@ export default function Services() {
               {services.map((service, index) => (
                 <div
                   key={index}
-                  className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+                  className="p-6 rounded-lg shadow-lg hover:shadow-xl transition duration-300"
+                  style={{ backgroundColor: "var(--blawhs-color)" }}
                 >
                   <div className="text-5xl mb-4 text-center">
                     {service.icon}
                   </div>
-                  <h3 className="text-2xl font-semibold text-gray-800 mb-2 text-center">
+                  <h3 className="text-2xl font-semibold  mb-2 text-center">
                     {service.title}
                   </h3>
-                  <p className="text-gray-600 text-center">
+                  <p className=" text-center">
                     {service.description}
                   </p>
                 </div>
