d813f9927a9a5027b860cc04d2f07696e23495a8
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-07-25 15:19:41 +0300

    update

--- app/not-found.js
+++ app/not-found.js
@@ -12,7 +12,7 @@ function notFound() {
             placeContent: "center",
           }}
         >
-          <div class="bg-white pt-6 sm:pt-12 pb-12 sm:pb-18">
+          <div class="pt-6 sm:pt-12 pb-12 sm:pb-18" style={{backgroundColor:'var(--main-alt-color1)',transition:'0.7s'}}>
             <div class="container mx-auto">
               <div class=" lg:w-2/3 xl:w-1/2 mx-auto flex flex-col items-center justify-center px-6 sm:px-16">
                 <div class="w-full pb-full relative">
@@ -25,12 +25,12 @@ function notFound() {
                     load
                   />
                 </div>{" "}
-                <h4 class="text-gray-900 text-3xl mb-2 mt-4 font-bold text-center">
+                <h4 class="text-3xl mb-2 mt-4 font-bold text-center" style={{color:'var(--blawh-color)',transition:'0.7s'}}>
                   Oops! Page not found
                 </h4>{" "}
                 <p
-                  class="text-gray-700 mb-4 text-center leading-relaxed"
-                  style={{ fontSize: "13px" }}
+                  class="mb-4 text-center leading-relaxed"
+                  style={{ fontSize: "13px",color:'var(--blawh-color)',transition:'0.7s'}}
                 >
                   The page you are trying to access does not exist or has been
                   moved.
