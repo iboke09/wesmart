943099c88f3be12d2101192d34e13ca3c48a1c34
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-01 11:57:21 +0300

    update

--- app/_component/Landing/Landing.jsx
+++ app/_component/Landing/Landing.jsx
@@ -4,10 +4,10 @@ function Landing({ children }) {
   return (
     <div style={{ width: "100vw" }} className="landing">
       {children}
-      <div className="wave wave1"></div>
+      {/* <div className="wave wave1"></div>
       <div className="wave wave2"></div>
       <div className="wave wave3"></div>
-      <div className="wave wave4"></div>
+      <div className="wave wave4"></div> */}
     </div>
   );
 }
--- app/services/page.js
+++ app/services/page.js
@@ -4,73 +4,46 @@ import Navbar from "../_component/Navbar/Navbar";
 import Tagarob1 from "../_component/Tacarob1/Tagarob1";
 import React from "react";
 import styles from "./Services.module.css";
-
-const services = [
-  {
-    title: "Web Development",
-    description:
-      "We create responsive and dynamic websites tailored to your needs.",
-    icon: "🌐",
-  },
-  {
-    title: "E-commerce",
-    description:
-      "Build and grow your online store with our custom e-commerce solutions.",
-    icon: "🛒",
-  },
-  {
-    title: "UI/UX Design",
-    description:
-      "Enhance user experience with our top-notch UI/UX design services.",
-    icon: "🎨",
-  },
-  {
-    title: "SAP Consulting",
-    description:
-      "Optimize your business processes with our expert SAP consulting services.",
-    icon: "💼",
-  },
-];
+import Landing from "../_component/Landing/Landing";
+import Image from "next/image";
+import image from "../img/FrameHero.svg";
 
 export default function Services() {
   return (
     <>
-      <div style={{}}>
-        <div
-          className="py-20"
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg,rgba(0, 194, 255, 1) 0%,rgba(0, 51, 128, 1) 100%)",
-            paddingTop: "200px",
-          }}
-        >
-          <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" style={{color:"var(--blawh-color)"}}>
-            <h2 className="text-4xl font-extrabold text-white text-center mb-12">
-              Our Services
-            </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-              {services.map((service, index) => (
-                <div
-                  key={index}
-                  className="p-6 rounded-lg shadow-lg hover:shadow-xl  duration-300"
-                  style={{ backgroundColor: "var(--main-alt-color1)" ,transition:'0.7s'}}
-                >
-                  <div className="text-5xl mb-4 text-center">
-                    {service.icon}
-                  </div>
-                  <h3 className="text-2xl font-semibold  mb-2 text-center">
-                    {service.title}
-                  </h3>
-                  <p className=" text-center">
-                    {service.description}
-                  </p>
-                </div>
-              ))}
+      <>
+        <Landing>
+          <div className="textS container">
+            <div
+              className="centere"
+              style={{ textAlign: "left", top: "65px", position: "relative" }}
+            >
+              <h2>Our Services</h2>
+              <p style={{ color: "var(--blawh-color)" }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio
+                amet.
+                <br /> facilisis tortor malesuada malesuada.
+              </p>
+              <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 top-20 relative">
+                <div className="of" >ERP Systems</div>
+                <div className="act">Web Design</div>
+                <div className="of">Skada Systems</div>
+                <div className="act">Mobile App</div>
+              </div>
+            </div>
+            <div className="tawsitS" style={{top:'30px'}}>
+              <Image
+                src={image}
+                alt="alt"
+                width={450}
+                height={200}
+                loading="eager"
+              />
             </div>
           </div>
-        </div>
+        </Landing>
         <Tagarob1 />
-      </div>
+      </>
     </>
   );
 }
