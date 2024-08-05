d6739eda8182509adb855bd2cd4c9362549cdd9a
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-05 13:55:33 +0300

    update

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,9 +4,8 @@ import { useState } from "react";
 import styles from "./ContactForm.module.css";
 import Image from "next/image";
 import img from "../img/brand communication-amicoo.png";
-import Footer from "../_component/Footer/Footer"
-
-;
+import Footer from "../_component/Footer/Footer";
+import Landing from "../_component/Landing/Landing";
 const Contact = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -29,77 +28,79 @@ const Contact = () => {
 
   return (
     <>
-      <div className="why" style={{ height: "100vh" }}>
-        <div style={{ top: "110px", position: "relative" }}>
-          <section>
-            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-              <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                <div className="relative h-64  rounded-lg sm:h-80 lg:order-last lg:h-full why1">
-                  <div className="locals" style={{ position: "relative" }}>
-                    <Image src={img} alt="Sap" width={1000} height={100} />
+      <div className="contactP">
+        <Landing className="olurmu">
+          <div style={{ top: "110px", position: "relative" }}>
+            <section>
+              <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+                <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+                  <div className="relative h-64  rounded-lg sm:h-80 lg:order-last lg:h-full why1">
+                    <div className="locals" style={{ position: "relative" }}>
+                      <Image src={img} alt="Sap" width={1000} height={100} />
+                    </div>
                   </div>
-                </div>
 
-                <div className="lg:py-24 muzz">
-                  <div className={styles.contactFormContainer}>
-                    <h2 className="contact_title">Contact us</h2>
-                    <form
-                      action={"http://localhost:8000/server.php"}
-                      onSubmit={(event) => handleSubmit(event)}
-                      className={styles.contactForm}
-                      method="POST"
-                    >
-                      <div className={styles.inputGroup}>
-                        <input
-                          type="text"
-                          id="name"
-                          name="name"
-                          value={formData.name}
-                          onChange={handleChange}
-                          placeholder="Name"
-                          required
-                          className={styles.input}
-                        />
-                      </div>
-                      <div className={styles.inputGroup}>
-                        <input
-                          type="email"
-                          id="email"
-                          name="email"
-                          value={formData.email}
-                          onChange={handleChange}
-                          placeholder="Email"
-                          required
-                          className={styles.input}
-                        />
-                      </div>
-                      <div className={styles.inputGroup}>
-                        <textarea
-                          id="message"
-                          name="message"
-                          value={formData.message}
-                          onChange={handleChange}
-                          placeholder="Message"
-                          required
-                          className={styles.textarea}
-                        />
-                      </div>
-                      <button type="submit" className={styles.button}>
-                        Send Message
-                      </button>
-                    </form>
+                  <div className="lg:py-24 muzz">
+                    <div className={styles.contactFormContainer}>
+                      <h2 className="contact_title">Contact us</h2>
+                      <form
+                        action={"http://localhost:8000/server.php"}
+                        onSubmit={(event) => handleSubmit(event)}
+                        className={styles.contactForm}
+                        method="POST"
+                      >
+                        <div className={styles.inputGroup}>
+                          <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="Name"
+                            required
+                            className={styles.input}
+                          />
+                        </div>
+                        <div className={styles.inputGroup}>
+                          <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Email"
+                            required
+                            className={styles.input}
+                          />
+                        </div>
+                        <div className={styles.inputGroup}>
+                          <textarea
+                            id="message"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            placeholder="Message"
+                            required
+                            className={styles.textarea}
+                          />
+                        </div>
+                        <button type="submit" className={styles.button}>
+                          Send Message
+                        </button>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-        </div>
-        <div
-          style={{ top: "200px", position: "relative" }}
-          className="footer_top"
-        >
-          <Footer />
-        </div>
+            </section>
+          </div>
+          <div
+            style={{ top: "200px", position: "relative" }}
+            className="footer_top"
+          >
+            <Footer />
+          </div>
+        </Landing>
       </div>
     </>
   );
