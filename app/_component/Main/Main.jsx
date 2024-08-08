7fc9b38b492a6071df9e1734df63024aba70de5c
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-08 19:02:55 +0300

    update

--- app/_component/Main/Main.jsx
+++ app/_component/Main/Main.jsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useEffect, useState }  from "react";
+'use client'
+import React from "react";
 import { motion } from "framer-motion";
 import image from "../../img/FrameHero.svg";
 import Link from "next/link";
@@ -23,26 +23,7 @@ const Main = () => {
       opacity: 1,
     },
   };
-
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 767);
-    };
-
-    // Set initial state
-    handleResize();
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup event listener
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
+  
   return (
     <div className="text container">
       <div
@@ -57,32 +38,28 @@ const Main = () => {
           initial="hidden"
           animate="visible"
         >
-          {isMobile
-            ? text
-            : text.split("").map((char, index) => (
-                <motion.span key={index} variants={spanVariants}>
-                  {char}
-                </motion.span>
-              ))}
+          {text.split("").map((char, index) => (
+            <motion.span key={index} variants={spanVariants}>
+              {char}
+            </motion.span>
+          ))}
         </motion.p>
         <Link
           className="pro-btn animate__animated animate__zoomInLeft"
           href="/contact"
         >
-          <span style={{ zIndex: "1000", fontWeight: "normal" }}>
-            contact us
-          </span>
+          <span style={{ zIndex: "1000" ,fontWeight:'normal'}}>contact us</span>
         </Link>
         <Link
           className="bad-btn animate__animated animate__zoomInRight"
           href="#sap"
-          style={{ fontWeight: "normal" }}
+          style={{fontWeight:'normal'}}
         >
           learn more
         </Link>
       </div>
       <div className="tawsit">
-        <Image src={image} alt="alt" width={450} height={200} loading="eager" />
+        <Image src={image} alt="alt" width={450} height={200}  loading="eager"/>
         {/* <div class="pyramid-loader">
           <div class="wrapper">
             <span class="side side1"></span>
@@ -95,6 +72,6 @@ const Main = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Main;
