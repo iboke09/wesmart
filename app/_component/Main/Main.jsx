95602478431983804462fe4fd89364dcb5dddcd1
iboke09 <142616750+iboke09@users.noreply.github.com>
2024-08-09 18:46:04 +0300

    update

--- app/_component/Main/Main.jsx
+++ app/_component/Main/Main.jsx
@@ -1,27 +1,13 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import image from "../../img/FrameHero.svg";
 import Link from "next/link";
 import Image from "next/image";
 
 const Main = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768); // يعتبر أي عرض شاشة أقل من أو يساوي 768 بكسل كجهاز محمول
-    };
-
-    handleResize(); // تعيين الحالة عند التحميل الأولي
-    window.addEventListener("resize", handleResize); // الاستماع لتغير حجم الشاشة
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   const text =
     "Unlock your potential with our expert SAP solutions.Streamline operations and boost efficiency. Enhance your growth with exceptional web design and engaging graphics designs.";
-  
   const pVariants = {
     hidden: { opacity: 0 },
     visible: {
@@ -31,25 +17,20 @@ const Main = () => {
       },
     },
   };
-
   const spanVariants = {
     hidden: { opacity: 0 },
     visible: {
       opacity: 1,
     },
   };
-
   useEffect(() => {
-    if (!isMobile) {
-      setTimeout(() => {
-        let la = document.querySelector(".first1");
-        if (la) {
-          la.classList.add("animate");
-        }
-      }, 5000);
-    }
-  }, [isMobile]);
-
+    setTimeout(() => {
+      let la = document.querySelector(".first1");
+      if (la) {
+        la.classList.add("animate");
+      }
+    }, 5000);
+  }, []);
   return (
     <div className="text container">
       <div
@@ -64,16 +45,11 @@ const Main = () => {
           initial="hidden"
           animate="visible"
         >
-          {!isMobile ? (
-              text.split("").map((char, index) => (
-                <motion.span key={index} variants={spanVariants}>
-                  {char}
-                </motion.span>
-              ))
-            ) : (
-              <p>{text}</p> // محتوى ثابت للأجهزة المحمولة
-            )}
-          
+          {text.split("").map((char, index) => (
+            <motion.span key={index} variants={spanVariants}>
+              {char}
+            </motion.span>
+          ))}
         </motion.p>
         <Link
           className="pro-btn animate__animated animate__zoomInLeft"
@@ -93,6 +69,15 @@ const Main = () => {
       </div>
       <div className="tawsit">
         <Image src={image} alt="alt" width={450} height={200} loading="eager" />
+        {/* <div class="pyramid-loader">
+          <div class="wrapper">
+            <span class="side side1"></span>
+            <span class="side side2"></span>
+            <span class="side side3"></span>
+            <span class="side side4"></span>
+            <span class="shadow"></span>
+          </div>
+        </div> */}
       </div>
     </div>
   );
